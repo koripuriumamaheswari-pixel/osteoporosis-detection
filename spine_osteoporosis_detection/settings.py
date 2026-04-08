5e050218ba8dfd3605ff2a91ed92aceb140fdf45
koripuriumamaheswari-pixel <koripuriumamaheswari@gmail.com>
2026-04-08 14:24:09 +0530

    fixed allowed hosts

--- spine_osteoporosis_detection/settings.py
+++ spine_osteoporosis_detection/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-yk^l@uyn77bv*!5sx=z869dhwxx#rj!nlpvz3hj(ai^862w-!y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "osteoporosis-detection-u51p.onrender.com",
+    "localhost",
+    "127.0.0.1"
+]
 
 
 # Application definition
